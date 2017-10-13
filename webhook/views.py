c79e205b8ed1696137487e8fcb1da8e765fee084
yunyu <zhangyunyu@wanmeizhensuo.com>
2017-10-13 16:12:35 +0800

    fix

--- webhook/views.py
+++ webhook/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
-
 # Create your views here.
+import json
+
+import requests
+
+
+def send_dingding(res):
+    data = {
+        "msgtype": "markdown",
+        "markdown": {
+            "title": res.get('title', ''),
+            "text": "#### {}\n".format(res.get('title', ''))+
+                    "\n".join(["- {}".format(json.dumps(match)) for match in res.get("evalMatches")]) +
+                    "\n> [view]({link}) \n".format(link=res.get("ruleUrl", ""))
+        },
+    }
+    x = requests.post(
+        url="https://oapi.dingtalk.com/robot/send?access_token=f43fa72e23241abfa5c5c22f32b85ea1e34fce11bd05276ad795d3bf420f2c5f",
+        json=data
+    )
+    print(x.content)
+
 
 def webhook(request):
     print(request.POST.__dict__)
+    res = json.loads(request.body.decode())
+    send_dingding(res)
